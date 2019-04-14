bd9ad5300248c208d81204abbfbd119fdfb27cee
nhocchoda <nhocchoda@gmail.com>
2019-04-14 23:31:14 +0700

    create class KeyEventHandler with 3 state: key down, key state, key on

--- NinjaGaiden/NinjaGaiden/game.h
+++ NinjaGaiden/NinjaGaiden/game.h
@@ -3,6 +3,15 @@
 #include "d3d9.h"
 #include "d3dx9.h"
 
+/*cac su kien nhan phim, chia lam 3 giai doan*/
+class CKeyEventHandler 
+{
+	virtual void KeyState(BYTE *state) = 0;   //key dang duoc giu
+	virtual void OnKeyDown(int keycode) = 0; //sau khi bat dau nhan xuong
+	virtual void OnKeyUP(int keycode) = 0; // sau khi tha ra
+};
+
+
 class Cgame
 {
 private:
@@ -21,7 +30,6 @@ public:
 	void Init(HWND hWnd); //ham tao va thiet lap device va backbuffer
 	static Cgame *GetInstance();
 	
-
 	void Draw(float x, float y, LPDIRECT3DTEXTURE9 texture, int left, int top, int right, int bottom,int vt);
 
 	LPDIRECT3DDEVICE9 GetDirect3DDevice() { return this->d3ddv; }
